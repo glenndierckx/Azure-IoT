c0126b41893ebe3efc60a7108db2e5bd7a6957c4
Glenn Dierckx <glenn@windstyle.be>
2016-02-24 23:23:51 +0100

    added comments

--- src/EventHandler/Program.cs
+++ src/EventHandler/Program.cs
@@ -16,52 +16,45 @@ namespace EventHandler
     {
         private static JobHost _host;
         private static EventHubClient _eventHubClient;
+
         static string connectionString = "HostName=azure-iot-demo.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Zma9R1DQz5TrXzIOggualsAm1OxZp4/KFGLq8Fbce70=";
         static string iotHubD2cEndpoint = "messages/events";
 
         static void Main(string[] args)
         {
             _eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            // create listener task per partition
             Task.WaitAll(_eventHubClient.GetRuntimeInformation().PartitionIds.Select(Listen).ToArray());
         }
+
         static async Task Listen(string partitionId)
         {
             Console.WriteLine("Listening to partition " + partitionId);
+
+            // receive all events after UtcNow (replay events)
             var receiver = await _eventHubClient.GetDefaultConsumerGroup().CreateReceiverAsync(partitionId, DateTime.UtcNow);
             while (true)
             {
+                // wait for a event to receive
                 var eventData = await receiver.ReceiveAsync();
-                if (!HasEventData(eventData)) continue;
+                if (eventData == null) continue;
 
+                // read event data
                 var json = Encoding.ASCII.GetString(eventData.GetBytes());
                 var powerUsage = JsonConvert.DeserializeObject<PowerUsage>(json);
-                var deviceId = GetDeviceIdFromMessage(eventData);
+                var deviceId = eventData.SystemProperties["iothub-connection-device-id"].ToString();
 
                 Console.WriteLine("Event received: " + json);
 
-                var httpClient = new HttpClient();
+                // post to signalr (notify web clients)
                 var signalrRequest = new DevicePowerUsage
                 {
                     Id = Guid.Parse(deviceId),
                     Value = powerUsage.Value
                 };
-                await PostSignalrRequest(httpClient, signalrRequest);
+                await HttpService.Post("api/deviceupdate", signalrRequest);
             }
-        }
-
-        private static async Task PostSignalrRequest(HttpClient httpClient, DevicePowerUsage signalrRequest)
-        {
-            await HttpService.Post("api/deviceupdate", signalrRequest);
-        }
-
-        private static string GetDeviceIdFromMessage(EventData eventData)
-        {
-            return eventData.SystemProperties["iothub-connection-device-id"].ToString();
-        }
-
-        private static bool HasEventData(EventData eventData)
-        {
-            return eventData != null;
         }
     }
 }
