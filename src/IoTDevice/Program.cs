94a05161aea7014098caab976b9520410161d069
Glenn Dierckx <glenn@windstyle.be>
2016-02-25 08:04:30 +0100

    device registration

--- src/IoTDevice/Program.cs
+++ src/IoTDevice/Program.cs
@@ -1,9 +1,40 @@
-﻿namespace FakeDevice
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Shared;
+
+namespace FakeDevice
 {
     class Program
     {
         static void Main()
         {
+            var myDevice = GetOrCreateDevice();
+        }
+
+        static MyDevice GetOrCreateDevice()
+        {
+            MyDevice thisDevice = null;
+            var thisDeviceFile = "data.json";
+            if (File.Exists(thisDeviceFile))
+            {
+                var json = File.ReadAllText(thisDeviceFile, Encoding.ASCII);
+                var data = JsonConvert.DeserializeObject<MyDevice>(json);
+                try
+                {
+                    thisDevice = HttpService.Get<MyDevice>("api/devices/" + data.Id).Result;
+                }
+                catch (Exception) // bad developer
+                {
+                }
+            }
+            if (thisDevice == null)
+            {
+                thisDevice = HttpService.Post<MyDevice>("api/devices", new {}).Result;
+            }
+            File.WriteAllText(thisDeviceFile, JsonConvert.SerializeObject(thisDevice));
+            return thisDevice;
         }
     }
 }
